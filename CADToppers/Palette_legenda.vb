560462329aef354f33e8f08f0cc5450ec7bdb1f7
roynijkamp <roynijkamp@roynijkamp.nl>
2014-12-02 10:39:15 +0100

    Small fixes

--- CADToppers/Palette_legenda.vb
+++ CADToppers/Palette_legenda.vb
@@ -3,7 +3,16 @@ Imports Autodesk.AutoCAD.DatabaseServices
 Imports Autodesk.AutoCAD.EditorInput
 Imports Autodesk.AutoCAD.Geometry
 Imports System.Windows.Forms
+'CADToppers.com AutoCAD Plugin Copyright (C) 2014 Roy Nijkamp
 
+'This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; 
+'either version 2 of the License, or (at your option) any later version.
+
+'This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+'without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+'You should have received a copy of the GNU General Public License along with this program; 
+'if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 Public Class Palette_legenda
 
     Private Sub cmdPlaatsLegenda_Click(sender As Object, e As EventArgs) Handles cmdPlaatsLegenda.Click
